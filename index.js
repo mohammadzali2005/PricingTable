2419d8d053652bf6ef16e5dc22b585d1ec18df09
mzking <171434441+mohammadzali2005@users.noreply.github.com>
2025-09-07 19:10:14 +0300

    Added files
    added HTML, CSS and JS + zip files

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.querySelectorAll('tr:not(tr:first-child)').forEach(tableRow => {
+//     tableRow.addEventListener('mouseover', () => {
+
+//         const willChangeElements = {
+//             planTitles : document.querySelectorAll('.planTitle'),
+//             planPrices : document.querySelectorAll('.planPrice'),
+//             planTimes : document.querySelectorAll('.planTime'),
+//             tableColElements : document.querySelectorAll('tr:not(tr:first-child) td:not(td:last-child)'),
+//             tableColLastChild : document.querySelectorAll('tr:not(tr:first-child) td:last-child')
+//         }
+
+//         console.log('Working');
+
+//     });
+// })
